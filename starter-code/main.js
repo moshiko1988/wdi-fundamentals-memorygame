ec2591c7f7f64e60afc48b51d2036f2d62660438
moshiko1988 <moshiko1988@gmail.com>
2016-12-13 17:39:03 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+cardOne == cardTwo
+cardThree == cardFour
+
+if (cardOne === cardTwo) {
+	alart("You found a match!")
+} else {
+	alart("Sorry, Try again.")
+
+}
+if (cardThree === cardFour) {
+	alart("You found a match!")
+} else {
+	alart("Sorry, Try again.")
+}
